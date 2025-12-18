61727ea66c3ce05a8852fe29188302d71fb6977b
gaur44 <agrajgaur3@gmail.com>
2025-12-18 11:21:16 +0530

    Add problem solutions

---
+++ core-java-practice/gcr-codebase/programming-elements/HarryAgeCalculator.java
@@ -0,0 +1,9 @@
+public class HarryAgeCalculator {
+    public static void main(String[] args) {
+        // Birth year 2000
+        int birthYear = 2000;
+        int currentYear = 2024;
+        int age = currentYear - birthYear;
+        System.out.println("age: " + age);
+    }
+}
---
+++ core-java-practice/gcr-codebase/programming-elements/SamAverageMarks.java
@@ -0,0 +1,9 @@
+public class SamAverageClass {
+    public static void main(String[] args) {
+        int mathMarks = 94;
+        int physicsMarks = 95;
+        int chemMarks = 96;
+        double averageMarks = (mathMarks + physicsMarks + chemMarks ) / 3;
+        System.out.println("Sam's average mark in PCM is: " + averageMarks);
+    }
+}
