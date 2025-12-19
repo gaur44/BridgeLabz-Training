47bc674c33baa32f56ee1f3f5d331b2e2a66e935
gaur44 <agrajgaur3@gmail.com>
2025-12-19 16:24:58 +0530

    [Agraj Gaur] Add.Added solutions for L1 problem 14-18

---
+++ core-java-practice/gcr-codebase/control-flow/MultiplicationTable.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int n = sc.nextInt();
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(n + " * " + i + " = " + (n * i));
+        }
+
+        sc.close();
+    }
+}
+
+
