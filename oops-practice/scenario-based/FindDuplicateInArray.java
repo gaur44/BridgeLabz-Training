7f847a98c5de50e3563092ad7170b8d45d6af5de
gaur44 <agrajgaur3@gmail.com>
2026-01-03 15:45:08 +0530

    [Agraj Gaur] Add.Added find duplicate elemnets scenario based problem

---
+++ oops-practice/scenario-based/FindDuplicateInArray.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Random;
+
+public class FindDuplicateInArray {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+        Random rand = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(300); 
+        }
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = i+1; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    System.out.println("Duplicate Elements at:  " + "index " + i + " and index " + j);
+                }
+            }
+        }
+        System.out.println("No duplicates found");
+    }
+}
